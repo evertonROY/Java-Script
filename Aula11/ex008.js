d2d94188d937b9a9b7f4c66e481d86b65e3973de
evertonROY <75455579+evertonROY@users.noreply.github.com>
2021-12-24 17:34:45 -0300

    Update ex008.js

--- Aula11/ex008.js
+++ Aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou')
+var vel = 85
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
