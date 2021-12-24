7ceaaa3c64d579644f9f46883a046b23490e87e2
evertonROY <75455579+evertonROY@users.noreply.github.com>
2021-12-24 17:52:55 -0300

    Create ex009.js

---
+++ Aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'brasil'
+var a = `${país.toLocaleUpperCase()}`
+console.log(`Vivendo em ${país}`)
+if(a == 'BRASIL')
+{
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
