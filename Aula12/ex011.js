87d4b335ed8fc64d3488a0d29a3d9c3c7abddf3a
evertonROY <75455579+evertonROY@users.noreply.github.com>
2021-12-26 04:22:38 -0300

    Update ex011.js

--- Aula12/ex011.js
+++ Aula12/ex011.js
@@ -1,9 +1,9 @@
-var idade = 15
+var idade = 65
 console.log(`Você tem ${idade} anos!`)
-if(idade >= 18){
+if (idade >= 18 && idade <65) {
     console.log('Voto Obrigatório')
-}else{
-    if(idade >= 16){
-        console.log('Vota desejável')
-    }else{console.log('Não vota')}
-}
+} else if (idade >= 16 && idade >= 65) {
+    console.log('Voto opcional')
+} else {
+    console.log('Não vota')
+}   
