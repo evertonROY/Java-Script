096d1f329887793bed07fe768d0a32e47c257603
evertonROY <75455579+evertonROY@users.noreply.github.com>
2021-12-30 00:20:00 -0300

    .

---
+++ Aula12ex/ex014/script.js
@@ -0,0 +1,8 @@
+var msg = document.querySelector('#msg')
+var fot = document.querySelector('#foto')
+var img = document.querySelector('#imagem')
+var data = new Date()
+var hora = data.getHours()
+function reload(){
+document.body.style.background = 'black'
+}
