7c8c6cb3023aa800ead279258d5dfea6dc47dd57
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-01-06 01:39:15 -0300

    .

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -1,8 +1,16 @@
-var msg = document.querySelector('#msg')
-var fot = document.querySelector('#foto')
-var img = document.querySelector('#imagem')
-var data = new Date()
-var hora = data.getHours()
-function reload(){
-document.body.style.background = 'black'
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    var sec = data.getSeconds()
+    msg.innerHTML = `Agora são ${hora}:${min}:${sec}`
+    if (hora >= 0 && hora <12){
+        opa.innerHTML = ('bom dia')
+    }else if(hora < 18){
+        opa.innerHTML = ('bom dtarde')
+    }else{
+        opa.innerHTML = ('bom noite')
+    }
 }
