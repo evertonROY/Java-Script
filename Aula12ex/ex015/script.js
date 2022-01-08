b62956a1b66efac30a2524839358702e50a4a8c0
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-01-08 03:59:09 -0300

    .

---
+++ Aula12ex/ex015/script.js
@@ -0,0 +1,22 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length < 4  || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createAttribute('img')
+        img.setAtribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else {
+            genero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos!`
+    }
+}
