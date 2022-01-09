56160c3a9ada531695bcbb47e3ca8e071797208b
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-01-09 01:38:20 -0300

    Update script.js

--- Aula12ex/ex015/script.js
+++ Aula12ex/ex015/script.js
@@ -9,33 +9,34 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
-        var img = document.createAttribute('img')
+        var img = document.createElement('img')
         img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
             if (idade <= 10){
-                //Criança
+                img.setAttribute('src', 'bebe-h.png')
             }else if (idade <= 22){
-                //Jovem
+                img.setAttribute('src', 'jovem-h.png')
             }else if (idade <= 59){
-                //Adulto
+                img.setAttribute('src', 'adulto-h.png')
             }else{
-                //Idoso
+                img.setAttribute('src', 'idoso-h.png')
             }
         } else {
             genero = 'Mulher'
             if (idade <= 10){
-                //Criança
+                img.setAttribute('src', 'bebe-m.png')
             }else if (idade <= 22){
-                //Jovem
+                img.setAttribute('src', 'jovem-m.png')
             }else if (idade <= 59){
-                //Adulto
+                img.setAttribute('src', 'adulto-m.png')
             }else{
-                //Idoso
+                img.setAttribute('src', 'idoso-m.png')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos!`
+        res.appendChild(img)
     }
 
 }
