1483c4e685dc03e2016d4ccedb2747e42a09bce5
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-01-09 01:12:30 -0300

    Update script.js

--- Aula12ex/ex015/script.js
+++ Aula12ex/ex015/script.js
@@ -3,14 +3,14 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
-    if (fano.value.length < 4  || fano.value > ano) {
+    if (fano.value.length < 4 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
         var img = document.createAttribute('img')
-        //img.setAttribute('id', 'foto')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
         } else {
@@ -19,5 +19,5 @@ function verificar() {
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos!`
     }
-    
+
 }
