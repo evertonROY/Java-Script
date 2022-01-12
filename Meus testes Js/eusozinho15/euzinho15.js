8127caa72468abd4f202bb6459a4ecb5692ec194
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-01-12 01:55:41 -0300

    .

--- Meus testes Js/eusozinho15/euzinho15.js
+++ Meus testes Js/eusozinho15/euzinho15.js
@@ -1,6 +1,9 @@
 function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
+    var hdd = document.getElementById('hdd')
+    var cop = document.getElementById('cop')
+    var bnt = document.getElementById('bnt')
 
     const { hour, minute, hour12  } = formatDate(new Date().toISOString())
 
@@ -11,15 +14,27 @@ function carregar() {
         const { hour, minute, hour12  } = formatDate(new Date().toISOString())
 
         msg.innerHTML = `Agora são ${hour}:${minute} ${hour12}`
-        if (hour >= 0 && hour <12){
+        if (hour < 4 ) {
+            img.src = 'fotomadrugada.png'
+            document.body.style.background = '#000'
+            bnt.innerHTML = 'Tenha uma otima madrugada!'
+        }
+        else if (hour < 12){
             img.src = 'fotomanha.png'
             document.body.style.background = '#FFE4B5'
+            cop.style.color = 'black'
+            hdd.style.color = 'black'
+            bnt.innerHTML = 'Um bom dia para você!'
         }else if(hour < 18){
             img.src = 'fototarde.png'
             document.body.style.background = '#FF8C00'
+            cop.style.color = 'black'
+            hdd.style.color = 'black'
+            bnt.innerHTML = 'Tenha uma boa tarde!'
         }else{
             img.src = 'fotonoite.png'
             document.body.style.background = '#2F4F7F'
+            bnt.innerHTML = 'Boa noite Zzz!'
         }
         msg.innerHTML = `Agora são ${hour}:${minute} ${hour12}`
     }, 100);//reload a cada 1 minutos!
