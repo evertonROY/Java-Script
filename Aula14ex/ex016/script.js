fae989cc6fb386f25cad5323e9e3ab186084a530
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-01-20 04:35:00 -0300

    .

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,14 +1,3 @@
-var ini = document.getElementById('inic')
-var fim = document.getElementById('fim')
-var pas = document.getElementById('pass')
-var res = document.getElementById('res')
-var n = 1
-
-function Contar() {
-    var fi = Number(fim.value)
-    do {
-        res.innerHTML = `opa ${fi}`
-        n++
-    }while (n < fi) 
-
+function contar() {
+    let ini = document.getElementById('txti')
 }
