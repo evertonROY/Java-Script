2139f1f9b9c08d4b6b33480ba3ab4f12f034bd08
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-08-20 19:15:47 -0300

    .

---
+++ apipromises/script.js
@@ -0,0 +1,92 @@
+const mypromises = new Promise((resolve, reject) => {
+    const nome = "Everton"
+
+    if(nome == 'Everton'){
+        resolve("EXATO")
+    }else{
+        reject("Errado!!!")
+    }
+})
+
+mypromises.then((data) => {
+    console.log(data)
+})
+
+// Envadeamento de then's para modificar os dados
+
+const mypromises2 = new Promise((resolve, reject) => {
+    const nome = "Everton"
+
+    if(nome == 'Everton'){
+        resolve("EXATO")
+    }else{
+        reject("Errado!!!")
+    }
+})
+
+mypromises2.then((data) => {
+    return data.toLowerCase()
+})
+.then((stringModificada) => {
+console.log(stringModificada)
+})
+
+// Retorno do catch 
+
+const mypromises3 = new Promise((resolve, reject) => {
+    const nome = "joao"
+
+    if(nome == 'Everton'){
+        resolve("EXATO")
+    }else{
+        reject("Errado!!!")
+    }
+});
+
+mypromises3.then((data) => {
+    console.log(data)
+}).catch((err) => {
+    console.log('Aconteceu um erro: ' + err)
+})
+
+// Resolver varias promessas
+
+ const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('p1 ok!')
+    }, 2000)
+   
+})
+const p2 = new Promise((resolve, reject) => {
+    resolve('p2 ok!')
+})
+
+
+const p3 = new Promise((resolve, reject) => {
+    resolve('p3 ok!')
+})
+
+const resolveAll = Promise.all([p1,p2,p3]).then((data) => {
+console.log(data)
+})
+
+// Resolver varias promessas
+
+const p4 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('p4 ok!')
+    }, 2000)
+   
+})
+const p5 = new Promise((resolve, reject) => {
+    resolve('p5 ok!')
+})
+
+
+const p6 = new Promise((resolve, reject) => {
+    resolve('p6 ok!')
+})
+
+const resolveAllRace = Promise.race([p4,p5,p6]).then((data) => {
+console.log(data)
+})
