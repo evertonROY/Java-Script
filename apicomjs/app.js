4af2800cda523a6b802b87d8d341d7cbc1dd0ec4
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-08-20 20:02:42 -0300

    Update app.js

--- apicomjs/app.js
+++ apicomjs/app.js
@@ -1,3 +1,11 @@
-const fetchpokemon = () => {
-    
+const fetchPokemon = () => {
+    const getPokemonurl = id = `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(pokemon => {
+        console.log(pokemon)
+    })
 }
+
+fetchPokemon()
