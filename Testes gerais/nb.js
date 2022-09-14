020916288ee59d4321f1b8eeddf93f0400f490ef
evertonROY <75455579+evertonROY@users.noreply.github.com>
2022-09-13 22:25:11 -0300

    .

---
+++ Testes gerais/nb.js
@@ -0,0 +1,9 @@
+function adcionar(){
+    var clc = Number(document.getElementById('calc').innerHTML) + 1
+    document.getElementById('calc').innerHTML = clc
+}
+
+function remover(){
+    var clc = Number(document.getElementById('calc').innerHTML) - 1
+    document.getElementById('calc').innerHTML = clc
+}
